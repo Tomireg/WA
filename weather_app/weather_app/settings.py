8d4965c28d39969e9070e9b86f901077f2ef3cf7
Tomireg <146027113+Tomireg@users.noreply.github.com>
2024-12-02 18:15:07 +0000

    new bug fix test

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -79,15 +79,7 @@ WSGI_APPLICATION = 'weather_app.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wadb_9ggv',  # Replace with your database name
-        'USER': 'wadb_9ggv_user',  # Replace with your database user
-        'PASSWORD': '2VmjFl9TU1dZvuURuuWmEK9sG0LmuIlo',  # Replace with your database password
-        'HOST': 'dpg-ct6s2t3qf0us738m0pa0-a',  # Replace with your Render database host (e.g., db.postgres.render.com)
-        'PORT': '5432',  # Default PostgreSQL port
-    }
-}
+    'default': dj_database_url.config(default='postgresql://wadb_9ggv_user:2VmjFl9TU1dZvuURuuWmEK9sG0LmuIlo@dpg-ct6s2t3qf0us738m0pa0-a/wadb_9ggv',        conn_max_age=600    )}
 
 
 
